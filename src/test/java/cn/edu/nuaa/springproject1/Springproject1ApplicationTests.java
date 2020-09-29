56c711dda8d541085a92593d073ac6d315bb487d
TSunT <suntao2020@nuaa.edu.cn>
2020-09-30 00:18:54 +0800

    initialize the project

---
+++ src/test/java/cn/edu/nuaa/springproject1/Springproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nuaa.springproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
